94bc69253bdeea21cb15036e2cf05573bff1c8a5
Ailton Rocha Cordeiro <arc48388528@gmail.com>
2025-08-13 20:27:10 -0300

    update
    Estudos JS

--- JavaScript/aula08/aula008.js
+++ JavaScript/aula08/aula008.js
@@ -1,1 +1,11 @@
+var vel = 12;
+console.log(`Seu carro está a ${vel} KM/h!`);
 
+if (vel > 60) { //condiçao simples. Se fosse somente com o if, com o else se torna composta.
+    console.log(`Sua velocidade foi ultrapassada! (MULTADO)`);
+} else {
+    console.log(`Velocidade de ${vel} KM/h é permitida.`);
+    console.log(`Boa viagem!`);
+}
+
+console.log(`Tome cuidado.`);
---
+++ JavaScript/aula08/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}. Coisa Boa!`)
+
+if (país == 'Brasil'){
+    console.log(`Morador, Brasileiro!`)
+}else{
+    console.log(`Estrangeiro.`)
+}

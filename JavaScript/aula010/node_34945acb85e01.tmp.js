a6eb09b9ca62a889fa2badce5c9fb6e22d1429be
Ailton Rocha Cordeiro <arc48388528@gmail.com>
2025-08-18 12:57:32 -0300

    update
    nova função

---
+++ JavaScript/aula010/node_34945acb85e01.tmp.js
@@ -0,0 +1,9 @@
+var idade = 16;
+
+if (idade < 16)  {
+    console.log("Você não pode votar!");
+} else if (idade >= 16 && idade < 18) {
+    console.log("Você pode votar, mas não é obrigatório.");
+} else if (idade >= 18) {
+    console.log("Você pode votar e é obrigatório.");
+}

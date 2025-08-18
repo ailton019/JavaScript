f7cd9d88ba8745faa88a2ff45f0b7d1e4f260f76
Ailton Rocha Cordeiro <arc48388528@gmail.com>
2025-08-18 13:08:26 -0300

    update aula
    aula, update

--- JavaScript/aula010/aula01.js
+++ JavaScript/aula010/aula01.js
@@ -1,8 +1,8 @@
-var idade = 16;
+var idade = 15;
 
 if (idade < 16)  {
-    console.log("Você não pode votar!");
-} else if (idade >= 16 && idade < 18) {
+    console.log("Você não pode votar! Por ser menor de idade.");
+} else if (idade < 18 || idade > 65) {
     console.log("Você pode votar, mas não é obrigatório.");
 } else if (idade >= 18) {
     console.log("Você pode votar e é obrigatório.");
